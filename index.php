4bf7ec630e4be89d94542efb63228fc782cad33e
Tran Anh Duc <anhduc.itspecialist@gmail.com>
2015-08-14 23:11:29 +0700

    sua file index

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
 <?php
 include_once 'controller/controller.php';
 $model = new model();
@@ -12,5 +5,3 @@ $view = new view();
 $controller = new controller($model, $view);
 echo $controller->action();
 ?>
-</body>
-</html>
